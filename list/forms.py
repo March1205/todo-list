8539f52a7a607d06e9d8503b21b3a7fa749f9f74
Marharyta Marchenko <ritanika12@gmail.com>
2024-05-21 15:20:24 +0200

    update TaskForm to render deadline as date picker

--- list/forms.py
+++ list/forms.py
@@ -3,6 +3,10 @@ from list.models import Task, Tag
 
 
 class TaskForm(forms.ModelForm):
+    deadline = forms.DateField(widget=forms.DateInput(
+        attrs={"type": "date"}
+    ))
+
     class Meta:
         model = Task
         fields = ("content", "deadline", "tags", "is_done")
