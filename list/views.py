ed9c420494da9e2d6b0f5e1cf335eef649a2f45f
Marharyta Marchenko <ritanika12@gmail.com>
2024-05-21 14:04:24 +0200

    add views for task and tag management

--- list/views.py
+++ list/views.py
@@ -1,3 +1,58 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from list.models import Task, Tag
+
+
+def index(request):
+    tasks = Task.objects.all().order_by("is_done")
+    return render(request, "list/index.html", {"tasks": tasks})
+
+
+class TagListView(generic.ListView):
+    model = Tag
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    form_class = TagForm
+    success_url = reverse_lazy("list:tag_list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    form_class = TagForm
+    success_url = reverse_lazy("list:tag_list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("list:tag_list")
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("list:index")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("list:index")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("list:index")
+
+
+class TaskToggleStatusView(generic.RedirectView):
+    pattern_name = "list:index"
+
+    def get_redirect_url(self, *args, **kwargs):
+        task = Task.objects.get(pk=self.kwargs["pk"])
+        task.is_done = not task.is_done
+        task.save()
+        return super().get_redirect_url(*args, **kwargs)
